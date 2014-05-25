31ca77f4b361fbb04a7c188bae051dcf1c4df506
Leonardo <leonardo@leonardocarlos.com.br>
2014-05-25 13:23:07 -0300

    Template Bispo Roberto Amaral

--- page.php
+++ page.php
@@ -1,21 +1,21 @@
 <?php get_header(); ?>
 <div id="container">
 				<div id="content">
-<?php the_breadcrumb(); ?><br />    				
+<?php the_breadcrumb(); ?><br />
 				<?php if ( have_posts() ):
 					while( have_posts() ): the_post(); ?>
-					
+
 					<div class="post">
 						<h1><a href="<?php the_permalink(); ?>" title="<?php esc_attr( the_title() ); ?>"><?php the_title(); ?></a></h1>
-													
-						<div class="entry"> 
+
+						<div class="entry">
 							<?php the_content(); ?>
-						</div> 
-						
+						</div>
+
 					</div><!-- .post -->
 
 					<?php // comments_template(); ?>
-					
+
 					<?php endwhile; ?>
 				<?php else: ?>
 					<?php get_template_part('no-results'); ?>
